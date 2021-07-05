22405befd590d2cf4be0c925883d468ba835ad75
Nico Molon <johnnicomolon@yahoo.com>
2021-07-05 21:43:03 +0800

    Added job counter

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ document.querySelector(".button__container")
         let text = document.getElementById("filter-jobs").value;
         getJobs().then(jobs => {
             let filteredJob = filterJobs(jobs, text);
-            showJobs(filteredJob);
-            // console.log("Filter jobs", filteredJob);
+            if(filteredJob.length === 0) {
+                showJobs(filteredJob);
+                console.log("0 Result");
+            }else {
+                showJobs(filteredJob);
+            }
         });
     })
 
@@ -37,34 +41,42 @@ function filterJobs(jobs, searchText) {
 }
 
 function showJobs(jobs) {
-    console.log("Jobs in showJobs", jobs)
+    console.log("JObs inside showjobs ", jobs);
     let jobsContainer = document.querySelector(".jobs-container");
+    let jobCount = document.querySelector(".job-count");
     let jobsHTML = "";
-    jobs.forEach(job => {
-  
-        jobsHTML += `
-            <div class="job-card">
-                <div class="top">
-                    <img src="${job.logo}" />
-                    <i class="material-icons">more_horiz</i>
-                </div>
-                <div class="rolename">
-                    <span>${job.roleName}</span>
-                </div>
-                <div class="description">
-                    <span>${job.requirements.content}</span>
-                </div>
-                <div class="buttons">
-                    <div class="button apply-now">
-                        Apply Now
+    let jobCountHTML = jobs.length;
+
+    if(jobs.length > 0) {
+        jobs.forEach(job => {
+            jobsHTML += `
+                <div class="job-card">
+                    <div class="top">
+                        <img src="${job.logo}" />
+                        <i class="material-icons">more_horiz</i>
                     </div>
-                    <div class="button">
-                        Message
+                    <div class="rolename">
+                        <span>${job.roleName}</span>
+                    </div>
+                    <div class="description">
+                        <span>${job.requirements.content}</span>
+                    </div>
+                    <div class="buttons">
+                        <div class="button apply-now">
+                            Apply Now
+                        </div>
+                        <div class="button">
+                            Message
+                        </div>
                     </div>
                 </div>
-            </div>
-        `
-    });
+            `
+        });
+    }else {
+        jobCountHTML = jobs.length;
+        console.log(jobCountHTML);
+    }
+    jobCount.innerHTML = jobCountHTML;
     jobsContainer.innerHTML = jobsHTML;
 }
 
